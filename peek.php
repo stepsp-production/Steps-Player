895f23eb2b1462eb7ab86cea8143aae996ab6432
stepsp-production <it@steps.net.sa>
2025-09-04 05:40:21 +0300

    Update peek.php

--- peek.php
+++ peek.php
@@ -1,5 +1,5 @@
 <?php
-$u = 'https://46.152.153.249/hls/live3/playlist.m3u8';
+$u = 'https://46.152.153.249/hls/live/playlist.m3u8';
 $h = @get_headers($u, 1);
 header('Content-Type: text/plain; charset=UTF-8');
 var_export($h);
